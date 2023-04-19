13e92f5b587557d5f24004924798f858850e8a31
Juan-E-M <juan.escobar.m@tecsup.edu.pe>
2023-04-18 20:11:07 -0500

    lab4

--- importar.js
+++ importar.js
@@ -1,50 +1,36 @@
 var http = require('http'),
     fs = require('fs');
-var parser = require('./parser_var.js'),
+    parser = require('./parser_var.js'),
     horas1 = require('./horas.js'),
-    diasFaltantes = require('./diasFaltantes.js');
-var p = parser.parse_vars;
-var datos = parser.batman;
-var formatos = horas1.horas;
-
-var rutas = {
-    '/inicio': 'inicio.html',
-    '/galeria': 'fotos.html',
-    '/': 'form.html'
-};
+    diasFaltantes = require('./diasFaltantes.js')
+    p = parser.parse_vars;
+    datos = parser.batman;
+    formatos = horas1.horas;
 
 http.createServer(function(req, res) {
-    var ruta = req.url;
+    fs.readFile('./form.html', function(err, html){
+        var html_string= html.toString();
 
-    
-        var archivo = rutas[ruta];
-        fs.readFile('./' + archivo, function(err, html) {
-            if (err) {
-                throw err;
+        var respuesta = p(req),
+        parametros = respuesta['parametros'],
+        valores =respuesta['valores'];
+
+        for(var i=0; i<parametros.length; i++){
+            var html_string = html_string.replace('{'+parametros[i]+'}', valores[i]);
+            if (parametros[i] == 'fecha'){
+                var cantidad = diasFaltantes.diasFaltantes(valores[i])
+                html_string = html_string.replace('{diasFaltantes}',cantidad.diasFaltantes)
             }
+        }
 
-            var html_string = html.toString();
+        html_string = html_string.replace('{identidad}', datos.identidad);
+        html_string = html_string.replace('{poder}', datos.poder);
+        html_string = html_string.replace('{hora12}', formatos.hora12);
+        html_string = html_string.replace('{hora24}', formatos.hora24);
 
-            var respuesta = p(req),
-                parametros = respuesta['parametros'],
-                valores =respuesta['valores'];
+        res.writeHead(200, {'Content-type': 'text'});
+        res.write(html_string);
+        res.end();
 
-            for(var i=0; i<parametros.length; i++){
-                var html_string = html_string.replace('{'+parametros[i]+'}', valores[i]);
-                if (parametros[i] == 'fecha'){
-                    var cantidad = diasFaltantes.diasFaltantes(valores[i])
-                    html_string = html_string.replace('{diasFaltantes}',cantidad.diasFaltantes)
-                }
-            }
-
-            html_string = html_string.replace('{identidad}', datos.identidad);
-            html_string = html_string.replace('{poder}', datos.poder);
-            html_string = html_string.replace('{hora12}', formatos.hora12);
-            html_string = html_string.replace('{hora24}', formatos.hora24);
-
-            res.writeHead(200, {'Content-type': 'text/html'});
-            res.write(html_string);
-            res.end();
-        });
-    
+    });
 }).listen(8080);
